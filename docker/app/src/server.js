aa7d90f88f24063e775d3654a4db3eb2b057a7db
juanma <jpinero@accedia.es>
2026-06-12 09:37:10 +0200

    acp change

--- docker/app/src/server.js
+++ docker/app/src/server.js
@@ -10,14 +10,42 @@ app.get('/tasks', (req,res) => {
 } );
 
 app.get('/tasks/:taskId', (req,res) => {
-    const id = Number(req.params.taskId);
-    const ret = TASKS.getTask(id);
+    let ret;
+    let isOk = true;
+    let status;
 
-    if (!ret.found) {
-        return res.status(404);
+    const errobj={
+        id: req.params.taskId,
+        error: ''
+    };
+
+    const id = Number(errobj.id);
+
+    if (isNaN(id)) {
+        errobj.error = `The id '${errobj.id}' is not a valid number.`;
+        isOk = false;
+    }
+    else {
+        ret = TASKS.getTask(id);
+        
+        if (ret.found) {
+            ret = ret.data;
+        }
+        else {
+            errobj.error = `There is no task with id='${errobj.id}'.`;
+            isOk = false;
+        }
     }
 
-    return res.json(ret.data);
+    if (isOk) {
+        status = 200;
+    }
+    else {
+        status = 404;
+        ret = errobj;
+    }
+
+    return res.status(status).json(ret);
 } );
 
 
